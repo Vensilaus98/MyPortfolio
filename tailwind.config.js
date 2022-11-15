795d88539422381ab1688d2f02c6ef8c0858843a
Vensilaus98 <vensilauskimario98@gmail.com>
2022-11-16 02:34:01 +0300

    Worked on Configurations and navbar

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+  ],
+  theme: {
+    //Configure Screen Sizes
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontFamily: {
+      oswald: ["oswald-bold"]
+    },
+    extend: {
+      //Add Project Colors
+      colors: {
+        primaryDark: "#003459",
+        primaryLight: "#003566",
+        AccentColor: "#F15BB5",
+      }
+    },
+  },
+  plugins: [],
+}
